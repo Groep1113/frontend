2e63c41a161eece2f460735faf60717b29dafaab
Peter Schriever <peterzen72@gmail.com>
2018-11-07 18:13:30 +0100

    added apollo-graphql Query example and QueryHOC helper

--- src/Components/App/index.js
+++ src/Components/App/index.js
@@ -1,21 +1,34 @@
 import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import QueryHOC from '../HOC/QueryHOC';
 import {
   Route, Switch, Redirect, withRouter
 } from 'react-router-dom';
 import HelloWorld from '../HelloWorld';
-
 import './App.css';
 
-export default class App extends Component {
-  state = { hello: '' }
+const query = gql`{
+  users {
+    id firstName lastName role email
+  }
+}`;
 
+@QueryHOC(query)
+export default class App extends Component {
   render() {
+    const { loading, error, data: {users} } = this.props.queryResults;
+    if (loading) return "Loading graphql query..";
+    if (error) return `GraphQL query resulted in error: ${error}`;
     return (
-      <div>
-        <Switch>
-          <Route component={HelloWorld} /> {/* Index page (/) */}
-        </Switch>
-      </div>
+      <>
+        <h2>Alle users van de backend gehaald;</h2>
+        <ul>{users.map(userToJSX)}</ul>
+      </>
     );
   }
 }
+
+const userToJSX = (u, i) => <li key={u.id + i}>
+  id: {u.id}, firstName: {u.firstName}, lastName: {u.lastName},
+  email: {u.email}, role: {u.role}
+</li>;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './Components/App';
+import ApolloClient from "apollo-boost";
 import { BrowserRouter, withRouter } from 'react-router-dom';
+import { ApolloProvider } from "react-apollo";
+import App from './Components/App';
 
 import './index.css' // global base css
 
+const client = new ApolloClient({
+  uri: "http://localhost:9000/graphql"
+});
+
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById('app')
+  <ApolloProvider client={client}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ApolloProvider>
+  ,document.getElementById('app')
 );
