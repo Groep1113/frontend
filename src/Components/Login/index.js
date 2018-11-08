bd54f9140c525967b4494ace2e2f5924f184c81f
Unknown <f.hoornveld@gmail.com>
2018-11-08 16:15:39 +0100

    in login implement onChange
    text fields can now be used

--- src/Components/Login/index.js
+++ src/Components/Login/index.js
@@ -2,32 +2,60 @@ import React, { Component } from 'react';
 import './login.css';
 
 export default class Login extends Component {
+    state = { email:"@htg.nl", password:"" };
     render() {
         return (
-            <div className="login">
-                <tr>
-                    <td>
-                        E-mail:
-                    </td>
-                    <td>
-                        &nbsp;&nbsp;
-                    </td>
-                    <td>
-                        <input type="text" name="email" value="@htg.nl" />
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        Wachtwoord:
-                    </td>
-                    <td>
-                        &nbsp;&nbsp;
-                    </td>
-                    <td>
-                        <input type="text" name="password" value="" />
-                    </td>
-                </tr>
-            </div>
+            <>
+                <div className="title">
+                    <b>Login pagina</b>
+                </div>
+                <div className="login">
+                    <table>
+                        <tbody>
+                            <tr>
+                                <td>
+                                    E-mail:
+                                </td>
+                                <td>
+                                    &nbsp;&nbsp;
+                                </td>
+                                <td>
+                                    <input type="text" name="email" value={this.state.email}
+                                        onChange={({ target: {value} }) => {
+                                            this.setState({ email: value });
+                                        }} />
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    Wachtwoord:
+                                </td>
+                                <td>
+                                    &nbsp;&nbsp;
+                                </td>
+                                <td>
+                                    <input type="password" name="password" value={this.state.password}
+                                        onChange={({ target: {value} }) => {
+                                            this.setState({ password: value });
+                                        }}/>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    &nbsp;
+                                </td>
+                                <td>
+                                    &nbsp;
+                                </td>
+                                <td>
+                                    <br />
+                                    <input type="submit" name="ok" value="OK" />
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </>
         );
     }
 }
