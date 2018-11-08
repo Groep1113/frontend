cd23b5e057400d61fb5ee3f70498e76a59889a74
Unknown <f.hoornveld@gmail.com>
2018-11-08 19:48:42 +0100

    welcome message added to navbar
    not yet only for welcome component

--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
 import './navbar.css';
+import gql from 'graphql-tag';
+import QueryHOC from '../HOC/QueryHOC';
+import { Route } from 'react-router-dom';
 
+const query = gql`{
+  user(id:1) {
+    firstName lastName email roles { name   }
+  }
+}`;
+
+@QueryHOC(query)
 export default class Navbar extends Component {
     render() {
+        const { loading, error, data} = this.props.queryResults;
+        if (loading) return "Loading graphql query..";
+        if (error) return `GraphQL query resulted in error: ${error}`;
+        const { user } = data;
         return (
             <div className="navbar">
-                <b>HTG</b>
+                <b>
+                    Welkom, <Route path='/' exact render={ () => <span> { user.firstName } </span>} />
+                </b>
             </div>
         );
     }
