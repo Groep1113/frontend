e486fc939ecbd17714987c0703642cdaa44dfb8b
Unknown <f.hoornveld@gmail.com>
2018-11-08 12:55:01 +0100

    remove graphql example from app

--- src/Components/App/index.js
+++ src/Components/App/index.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-import gql from 'graphql-tag';
-import QueryHOC from '../HOC/QueryHOC';
 import {
   Route, Switch, Redirect, withRouter
 } from 'react-router-dom';
@@ -11,33 +9,17 @@ import Welcome from '../Welcome';
 
 import './App.css';
 
-const query = gql`{
-  users {
-    id firstName lastName role email
-  }
-}`;
-
-@QueryHOC(query)
 export default class App extends Component {
   render() {
-    const { loading, error, data: {users} } = this.props.queryResults;
-    if (loading) return "Loading graphql query..";
-    if (error) return `GraphQL query resulted in error: ${error}`;
+      let header;
     return (
       <div>
           <Navbar />
-          <h2>Alle users van de backend gehaald;</h2>
-          <ul>{users.map(userToJSX)}</ul>
         <Switch>
           {/*<Route component={Login} /> /!* Index page (/) *!/*/}
-          <Route component={Welcome} /> {/* Index page (/) */}
+          <Route component={Welcome} /> /* Index page (/) */}
         </Switch>
       </div>
     );
   }
 }
-
-const userToJSX = (u, i) => <li key={u.id + i}>
-  id: {u.id}, firstName: {u.firstName}, lastName: {u.lastName},
-  email: {u.email}, role: {u.role}
-</li>;
