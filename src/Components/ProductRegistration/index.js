2f9994fe6675de4ff1793a22920a6834ebd69df4
Unknown <f.hoornveld@gmail.com>
2018-11-26 23:30:33 +0100

    adding button to productregistration

--- src/Components/ProductRegistration/index.js
+++ src/Components/ProductRegistration/index.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 import gql from 'graphql-tag';
 import './productregistration.css';
-import Table from '../Table';
+import Button from '@material-ui/core/Button';
+import AddIcon from '@material-ui/icons/Add';
+import { createMuiTheme, MuiThemeProvider } from '@material-ui/core';
 import QueryHOC from '../HOC/QueryHOC';
 
 const query = gql`{
@@ -10,6 +12,19 @@ const query = gql`{
     name code locations {code} recommended_stock 
   }
 }`;
+
+const theme = createMuiTheme({
+  // because we apparently use typography and it will be deprecated with the next release,
+  // to have a smooth transition:
+  typography: {
+    useNextVariants: true,
+  },
+  palette: {
+    primary: {
+      main: '#0a45c1',
+    },
+  },
+});
 
 const columnFormatting = ['name', 'code', ({ locations }) => locations.reduce((accum, { code }) => `${accum}, ${code}`, '').substring(2), 'recommended_stock'];
 const firstRowTable = ['Product', 'Type', 'Locatie', 'Aanbevolen voorraad'];
@@ -29,6 +44,11 @@ export default class ProductRegistration extends Component {
           <h3>
             Huidige producten:
           </h3>
+          <MuiThemeProvider theme={theme}>
+            <Button variant='fab' color='primary' className='add' onClick={this.handleClickOpen}>
+              <AddIcon />
+            </Button>
+          </MuiThemeProvider>
         </div>
         <Table data = {data.items} headers = {firstRowTable} columns = {columnFormatting}/>
       </div>
