ac70826575a86c28f893a0f21800dd6c097dfe4f
Unknown <f.hoornveld@gmail.com>
2018-11-26 23:31:09 +0100

    adding popup to add button

--- src/Components/ProductRegistration/index.js
+++ src/Components/ProductRegistration/index.js
@@ -5,7 +5,14 @@ import './productregistration.css';
 import Button from '@material-ui/core/Button';
 import AddIcon from '@material-ui/icons/Add';
 import { createMuiTheme, MuiThemeProvider } from '@material-ui/core';
+import Dialog from '@material-ui/core/Dialog/Dialog';
+import DialogTitle from '@material-ui/core/DialogTitle/DialogTitle';
+import DialogContent from '@material-ui/core/DialogContent/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText/DialogContentText';
+import DialogActions from '@material-ui/core/DialogActions/DialogActions';
+import TextField from '@material-ui/core/TextField';
 import QueryHOC from '../HOC/QueryHOC';
+import Table from '../Table';
 
 const query = gql`{
   items {
@@ -32,6 +39,20 @@ const firstRowTable = ['Product', 'Type', 'Locatie', 'Aanbevolen voorraad'];
 @QueryHOC(query)
 @withRouter
 export default class ProductRegistration extends Component {
+  state = {
+    open: false,
+  };
+
+  handleClickOpen = () => {
+    this.setState({
+      open: true,
+    });
+  };
+
+  handleClose = () => {
+    this.setState({ open: false });
+  };
+
   render() {
     const { loading, error, data } = this.props.queryResults;
     if (loading) return 'Loading graphql query..';
@@ -48,6 +69,52 @@ export default class ProductRegistration extends Component {
             <Button variant='fab' color='primary' className='add' onClick={this.handleClickOpen}>
               <AddIcon />
             </Button>
+            <Dialog
+              open={this.state.open}
+              onClose={this.handleClose}
+              aria-labelledby='alert-dialog-title'
+              aria-describedby='alert-dialog-description'
+            >
+              <DialogTitle id='alert-dialog-title'>Voeg een product toe:</DialogTitle>
+              <DialogContent>
+                <DialogContentText id='alert-dialog-description'>
+                  <TextField
+                    id='standard-name'
+                    label='Product'
+                    margin='normal'
+                  />
+                </DialogContentText>
+                <DialogContentText id='alert-dialog-description'>
+                  <TextField
+                    id='standard-name'
+                    label='Type'
+                    margin='normal'
+                  />
+                  <DialogContentText id='alert-dialog-description'>
+                  </DialogContentText>
+                  <TextField
+                    id='standard-name'
+                    label='Locatie'
+                    margin='normal'
+                  />
+                  <DialogContentText id='alert-dialog-description'>
+                  </DialogContentText>
+                  <TextField
+                    id='standard-name'
+                    label='Aanbevolen voorraad'
+                    margin='normal'
+                  />
+                </DialogContentText>
+              </DialogContent>
+              <DialogActions>
+                <Button onClick={this.handleClose} color="primary">
+                  Cancel
+                </Button>
+                <Button onClick={this.handleClose} color="primary" autoFocus>
+                  Ok
+                </Button>
+              </DialogActions>
+            </Dialog>
           </MuiThemeProvider>
         </div>
         <Table data = {data.items} headers = {firstRowTable} columns = {columnFormatting}/>
