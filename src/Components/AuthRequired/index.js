4ea7fcb49f6f9e4046524b5b201ed5fdc1b00ff8
Peter Schriever <peterzen72@gmail.com>
2018-11-28 11:44:46 +0100

    fixed endless redirect when logged out

--- src/Components/AuthRequired/index.js
+++ src/Components/AuthRequired/index.js
@@ -7,7 +7,7 @@ export default class AuthRequired extends Component {
     const { location: { pathname } } = this.props;
     const token = localStorage.getItem('authToken');
     if (token === null && pathname !== '/login') return <Redirect to="/login" />;
-    if (pathname === '/login') return <Redirect to="/" />;
+    if (token !== null && pathname === '/login') return <Redirect to="/" />;
 
     const { children } = this.props;
     return <div>{children}</div>;
