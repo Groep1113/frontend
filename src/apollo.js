3d9f2cf6bd0109dc51ee6157ac4654eaad4f227d
Peter Schriever <peterzen72@gmail.com>
2018-11-28 12:38:43 +0100

    apollo now listens to errors and logs out users with invalid tokens

--- src/apollo.js
+++ src/apollo.js
@@ -1,8 +1,8 @@
 import ApolloClient from 'apollo-client';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { HttpLink } from 'apollo-link-http';
-import { ApolloLink, concat } from 'apollo-link';
-// import { onError } from 'apollo-link-error'; // @TODO add error handling middleware
+import { ApolloLink } from 'apollo-link';
+import { onError } from 'apollo-link-error'; // @TODO add error handling middleware
 
 const httpLink = new HttpLink({
   // @TODO: backend url as config option or environment variable
@@ -10,9 +10,9 @@ const httpLink = new HttpLink({
 });
 
 const authMiddleware = new ApolloLink((operation, forward) => {
-  // add the HTTP Basic Auth to the headers
+  // add the HTTP Bearer Auth to the headers
   const token = localStorage.getItem('authToken');
-  if (!token) return forward(operation);
+  if (token === null) return forward(operation);
 
   operation.setContext({
     headers: {
@@ -23,8 +23,17 @@ const authMiddleware = new ApolloLink((operation, forward) => {
   return forward(operation);
 });
 
+const errorLink = onError(({ forward, graphQLErrors }) => {
+  if (graphQLErrors.filter(({ message }) => message === 'Unauthorized.')) {
+    localStorage.clear();
+    window.location.replace('/');
+  }
+
+  forward();
+});
+
 const client = new ApolloClient({
-  link: concat(authMiddleware, httpLink),
+  link: ApolloLink.from([errorLink, authMiddleware, httpLink]),
   cache: new InMemoryCache(),
 });
 export default client;
