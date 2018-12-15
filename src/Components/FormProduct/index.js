1b938e9b16f191c4fa7503c35776c55118f7894b
Unknown <f.hoornveld@gmail.com>
2018-12-15 14:27:53 +0100

    dialogue to formproduct

---
+++ src/Components/FormProduct/index.js
@@ -0,0 +1,143 @@
+import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import Fab from '@material-ui/core/Fab/Fab';
+import AddIcon from '@material-ui/core/SvgIcon/SvgIcon';
+import Dialog from '@material-ui/core/Dialog/Dialog';
+import DialogTitle from '@material-ui/core/DialogTitle/DialogTitle';
+import DialogContent from '@material-ui/core/DialogContent/DialogContent';
+import TextField from '@material-ui/core/TextField/TextField';
+import DialogActions from '@material-ui/core/DialogActions/DialogActions';
+import Button from '@material-ui/core/Button/Button';
+import MutationHOC from '../HOC/MutationHOC';
+
+const mutation = gql`  
+  mutation($code: String!, $locationId: Int!, $recommended_stock: Int!, $name: String!) {
+  createItem (
+    code: $code, 
+    locationId: $locationId, 
+    recommended_stock: $recommended_stock, 
+    name: $name) {
+      code
+    }
+  }`;
+
+@MutationHOC(mutation)
+export default class FormProduct extends Component {
+  state = {
+    open: false,
+    product: '',
+    type: '',
+    location: '',
+    recommendedStock: '',
+    submit: false,
+  }
+
+  handleClickOpen = this.handleClickOpen.bind(this)
+
+  handleClose = this.handleClose.bind(this)
+
+  handleChange = this.handleChange.bind(this)
+
+  handleClick = this.handleClick.bind(this)
+
+  handleClickOpen() {
+    this.setState({
+      open: true,
+    });
+  }
+
+  handleChange({ target: { value } }, k) {
+    this.setState({
+      [k]: value,
+    });
+  }
+
+  handleClick() {
+    this.setState({
+      open: false,
+    });
+    const {
+      product, type,
+    } = this.state;
+    let { location, recommendedStock } = this.state;
+    location = parseInt(location, 10);
+    recommendedStock = parseInt(recommendedStock, 10);
+    this.props.mutateFunc({
+      variables: {
+        type, location, recommendedStock, product,
+      },
+    });
+  }
+
+  handleClose() {
+    this.setState({ open: false });
+  }
+
+  render() {
+    return (
+      <div className='header'>
+        <h3>
+          Huidige producten:
+        </h3>
+        <Fab color='primary' className='add' onClick={this.handleClickOpen}>
+          <AddIcon>
+
+          </AddIcon>
+        </Fab>
+        <Dialog
+          className='dialogueWindow'
+          open={this.state.open}
+          onClose={this.handleClose}
+          aria-labelledby='alert-dialog-title'
+          aria-describedby='alert-dialog-description'
+        >
+          <DialogTitle id='alert-dialog-title'>Voeg een product toe:</DialogTitle>
+          <DialogContent className='dialogueContent'>
+            <div>
+              <TextField
+                id='product'
+                name='product[name]'
+                label='Product'
+                margin='normal'
+                onChange={e => this.handleChange(e, 'product')}
+              />
+            </div>
+            <div>
+              <TextField
+                id='type'
+                name='type'
+                label='Type'
+                margin='normal'
+                onChange={e => this.handleChange(e, 'type')}
+              />
+            </div>
+            <TextField
+              id='location'
+              name='location'
+              label='Locatie'
+              margin='normal'
+              onChange={e => this.handleChange(e, 'location')}
+            />
+            <div>
+              <TextField
+                id='stock'
+                name='stock'
+                label='Aanbevolen voorraad'
+                margin='normal'
+                onChange={e => this.handleChange(e, 'recommendedStock')}
+              />
+            </div>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={this.handleClose} color="primary">
+              Cancel
+            </Button>
+            <Button onClick={this.handleClick} color="primary" autoFocus>
+              Ok
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </div>
+    );
+  }
+}
--- src/Components/ProductRegistration/index.js
+++ src/Components/ProductRegistration/index.js
@@ -2,17 +2,9 @@ import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 import gql from 'graphql-tag';
 import './productregistration.css';
-import Button from '@material-ui/core/Button';
-import Fab from '@material-ui/core/Fab';
-import AddIcon from '@material-ui/icons/Add';
-import Dialog from '@material-ui/core/Dialog/Dialog';
-import DialogTitle from '@material-ui/core/DialogTitle/DialogTitle';
-import DialogContent from '@material-ui/core/DialogContent/DialogContent';
-import DialogActions from '@material-ui/core/DialogActions/DialogActions';
-import TextField from '@material-ui/core/TextField';
 import QueryHOC from '../HOC/QueryHOC';
 import Table from '../Table';
-import Mutation from '../Mutation';
+import FormProduct from '../FormProduct';
 
 const query = gql`{
   items {
@@ -26,46 +18,6 @@ const firstRowTable = ['Product', 'Type', 'Locatie', 'Aanbevolen voorraad'];
 @QueryHOC(query)
 @withRouter
 export default class ProductRegistration extends Component {
-  state = {
-    open: false,
-    product: '',
-    type: '',
-    location: '',
-    recommendedStock: '',
-    submit: false,
-  }
-
-  handleClickOpen = this.handleClickOpen.bind(this)
-
-  handleClose = this.handleClose.bind(this)
-
-  handleChange = this.handleChange.bind(this)
-
-  handleClick = this.handleClick.bind(this)
-
-  handleClickOpen() {
-    this.setState({
-      open: true,
-    });
-  }
-
-  handleChange({ target: { value } }, k) {
-    this.setState({
-      [k]: value,
-    });
-  }
-
-  handleClick() {
-    this.setState({
-      submit: true,
-      open: false,
-    });
-  }
-
-  handleClose() {
-    this.setState({ open: false });
-  }
-
   render() {
     const { loading, error, data } = this.props.queryResults;
     if (loading) return 'Gegevens worden geladen..';
@@ -74,73 +26,7 @@ export default class ProductRegistration extends Component {
     }
     return (
       <div className='register'>
-        <div className='header'>
-          <h3>
-            Huidige producten:
-          </h3>
-          <Fab color='primary' className='add' onClick={this.handleClickOpen}>
-            <AddIcon />
-          </Fab>
-          <Dialog
-            className='dialogueWindow'
-            open={this.state.open}
-            onClose={this.handleClose}
-            aria-labelledby='alert-dialog-title'
-            aria-describedby='alert-dialog-description'
-          >
-            <DialogTitle id='alert-dialog-title'>Voeg een product toe:</DialogTitle>
-            <DialogContent className='dialogueContent'>
-              <div>
-                <TextField
-                  id='product'
-                  name='product[name]'
-                  label='Product'
-                  margin='normal'
-                  onChange={e => this.handleChange(e, 'product')}
-                />
-              </div>
-              <div>
-                <TextField
-                  id='type'
-                  name='type'
-                  label='Type'
-                  margin='normal'
-                  onChange={e => this.handleChange(e, 'type')}
-                />
-              </div>
-              <TextField
-                id='location'
-                name='location'
-                label='Locatie'
-                margin='normal'
-                onChange={e => this.handleChange(e, 'location')}
-              />
-              <div>
-                <TextField
-                  id='stock'
-                  name='stock'
-                  label='Aanbevolen voorraad'
-                  margin='normal'
-                  onChange={e => this.handleChange(e, 'recommendedStock')}
-                />
-              </div>
-            </DialogContent>
-            <DialogActions>
-              <Button onClick={this.handleClose} color="primary">
-                  Cancel
-              </Button>
-              <Button onClick={this.handleClick} color="primary" autoFocus>
-                  Ok
-              </Button>
-            </DialogActions>
-          </Dialog>
-        </div>
-        <Mutation
-          product = {this.state.product}
-          type = {this.state.type}
-          location = {this.state.location}
-          stock = {this.state.recommendedStock}
-          submit = {this.state.submit} />
+        <FormProduct />
         <Table data = {data.items} headers = {firstRowTable} columns = {columnFormatting}/>
       </div>
     );
