589f44fb33ea70f8d007aa03523a06a66da07f0d
Unknown <f.hoornveld@gmail.com>
2019-01-11 16:36:55 +0100

    make components optional in mutation

--- src/Components/Item/AddItem.js
+++ src/Components/Item/AddItem.js
@@ -18,7 +18,7 @@ import MutationHOC from '../HOC/MutationHOC';
 import './additem.css';
 
 const mutation = gql`
-  mutation($code: String!, $location: Int!, $recommendedStock: Int!, $product: String!) {
+  mutation($code: String, $location: Int, $recommendedStock: Int, $product: String!) {
     createItem (
       code: $code,
       locationId: $location,
