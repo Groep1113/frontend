66b965b9dc9b74ef9a7cac07f737f88f0ec7fe98
Unknown <f.hoornveld@gmail.com>
2019-01-13 11:16:52 +0100

    categories delete function translated

--- src/Components/CategoriesView/index.js
+++ src/Components/CategoriesView/index.js
@@ -5,7 +5,7 @@ import withStyles from '@material-ui/core/styles/withStyles';
 import ListView from './list';
 // import EditView from './edit';
 // import CreateView from './create';
-// import DeleteView from './delete';
+import DeleteView from './delete';
 
 const styles = theme => ({
   root: {
@@ -22,9 +22,9 @@ export default class CategoriesView extends Component {
     const { classes } = this.props;
     return (
       <div className={classes.root}>
-        {/* <Route path="/locations/create" component={CreateView} /> */}
-        {/* <Route path="/locations/delete/:id" component={DeleteView} /> */}
-        {/* <Route path="/locations2/edit/:id" component={EditView} /> */}
+        {/* <Route path="/categories2/create" component={CreateView} /> */}
+        <Route path="/categories2/delete/:id" component={DeleteView} />
+        {/* <Route path="/categories2/edit/:id" component={EditView} /> */}
         <Route component={ListView} />
       </div>
     );
