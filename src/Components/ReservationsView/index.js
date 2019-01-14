ef9c78fdf1e835c97014bf9c0eefd49c049bd246
Unknown <f.hoornveld@gmail.com>
2019-01-14 14:22:05 +0100

    reservations edit function added

--- src/Components/ReservationsView/index.js
+++ src/Components/ReservationsView/index.js
@@ -3,7 +3,7 @@ import { Route } from 'react-router-dom';
 import withStyles from '@material-ui/core/styles/withStyles';
 
 import ListView from './list';
-// import EditView from './edit';
+import EditView from './edit';
 // import CreateView from './create';
 import DeleteView from './delete';
 
@@ -24,7 +24,7 @@ export default class ReservationsView extends Component {
       <div className={classes.root}>
         {/* <Route path="/reservations2/create" component={CreateView} /> */}
         <Route path="/reservations2/delete/:id" component={DeleteView} />
-        {/* <Route path="/reservations2/edit/:id" component={EditView} /> */}
+        <Route path="/reservations2/edit/:id" component={EditView} />
         <Route component={ListView} />
       </div>
     );
