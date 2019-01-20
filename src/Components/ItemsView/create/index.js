33466c1edb43cebf0b8f2a9578accf16f168fc8a
Unknown <f.hoornveld@gmail.com>
2019-01-20 17:58:38 +0100

    add select supplier functionality to createItem

--- src/Components/ItemsView/create/index.js
+++ src/Components/ItemsView/create/index.js
@@ -3,8 +3,18 @@ import { Redirect } from 'react-router-dom';
 import gql from 'graphql-tag';
 import withStyles from '@material-ui/core/styles/withStyles';
 import TextField from '@material-ui/core/TextField';
+import FormControl from '@material-ui/core/FormControl/FormControl';
+import Typography from '@material-ui/core/Typography/Typography';
+import Select from 'react-select';
 import MutationHOC from '../../HOC/MutationHOC';
 import GenericDialog from '../../Common/CRUD/GenericDialog';
+import QueryHOC from '../../HOC/QueryHOC';
+
+const query = gql`query {
+  suppliers { id name }
+  locations { id code }
+  categories { id name }
+}`;
 
 const mutation = gql`mutation($code: String, $recommendedStock: Int, $name: String!) {
     createItem (
@@ -18,6 +28,7 @@ const mutation = gql`mutation($code: String, $recommendedStock: Int, $name: Stri
 const styles = ({ spacing }) => ({
 });
 
+@QueryHOC(query)
 @MutationHOC(mutation)
 @withStyles(styles)
 export default class ItemCreate extends Component {
@@ -25,11 +36,21 @@ export default class ItemCreate extends Component {
     code: '',
     recommendedStock: '',
     name: '',
+    supplier: '',
+    locationIds: [],
+    categoryIds: [],
+    selectedOptionSupplier: '',
+    selectedOptionLocation: '',
+    selectedOptionCategory: '',
   };
 
   render() {
-    const { mutateResults: { loading, error, data }, mutateFunc } = this.props;
+    const { mutateResults: { loading, error, data }, queryResults, mutateFunc } = this.props;
     if (!loading && !error && data) return <Redirect to='/items' />;
+    if (queryResults.loading) return 'Gegevens worden geladen..';
+    if (queryResults.error) {
+      return <div>Er ging iets fout.<br />{error.message}</div>;
+    }
 
     return (
       <GenericDialog
@@ -50,7 +71,29 @@ export default class ItemCreate extends Component {
           id='recommendedStock' name='recommendedStock' label="Minimum voorraad" type='recommendedStock' margin='normal'
           value={this.state.recommendedStock}
           onChange={e => this.setState({ recommendedStock: e.target.value })} />
+        <FormControl>
+          <Typography variant="subtitle1" className='supplier'>
+            Leverancier
+          </Typography>
+          <Select
+            value={this.state.selectedOption}
+            options={elementsToOptions(queryResults.data.suppliers)}
+            onChange={(...all) => this.setState(
+              { supplier: all[0].value, selectedOptionSupplier: all[1].label },
+            )}
+          />
+        </FormControl>
       </GenericDialog>
     );
   }
+}
+
+function elementsToOptions(elements) {
+  const options = [];
+  let i = 1;
+  for (const element of elements) {
+    options.push({ key: i, value: element.id, label: element.name });
+    i += 1;
+  }
+  return options;
 }
