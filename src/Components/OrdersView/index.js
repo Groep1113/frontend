049ed9a0f0a2c5083fd15b28366f65db49f14e87
Unknown <f.hoornveld@gmail.com>
2019-01-21 12:22:19 +0100

    update

--- src/Components/OrdersView/index.js
+++ src/Components/OrdersView/index.js
@@ -3,7 +3,7 @@ import { Route } from 'react-router-dom';
 import withStyles from '@material-ui/core/styles/withStyles';
 
 import ListView from './list';
-// import EditView from './edit';
+import EditView from './edit';
 import CreateView from './create';
 import DeleteView from './delete';
 
@@ -24,7 +24,7 @@ export default class OrdersView extends Component {
       <div className={classes.root}>
         <Route path="/orders/create" component={CreateView} />
         <Route path="/orders/delete/:id" component={DeleteView} />
-        {/* <Route path="/orders/edit/:id" component={EditView} /> */}
+        <Route path="/orders/edit/:id" component={EditView} />
         <Route component={ListView} />
       </div>
     );
